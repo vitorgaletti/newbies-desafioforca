bd48e9fc9203dfd0b7e0bc54f67850fcd2ed5832
Vitor Galeti <vitor.galeti@levva.io>
2023-03-27 15:37:34 -0300

    :sparkles: Added play again

--- Program.cs
+++ Program.cs
@@ -1,71 +1,84 @@
-﻿using System;
-using System.Runtime.Intrinsics.Arm;
+﻿namespace DesafioForca;
 
-namespace DesafioForca;
+public class ForcaGame {
+    private static readonly string[] wordList = { "AZUL", "AMARELO", "PRETO", "VERDE", "VERMELHO", "BRANCO", "ROXO", "ROSA", "LARANJA" };
+    private readonly List<char> guessesList = new List<char>();
+    public string[] getWordList() {
+        return wordList;
+    }
+
+    public List<char> getGuessesList() {
+        return guessesList;
+    }
+
+}
 public class Program {
 
-    public static List<char> guessList = new List<char>();
-    public static int guessesNumber = 6;
+    public static int MAX_GUESSES = 6;
+    public static string playAgainInput = "";
     public static void Main(string[] args) {
 
-        string[] wordList = { "AZUL", "AMARELO", "PRETO", "VERDE", "VERMELHO", "BRANCO", "ROXO", "ROSA", "LARANJA" };
-        Random random = new Random();
-        int indexWord = random.Next(wordList.Length);
-        string randomWord = wordList[indexWord];
+        ForcaGame forcaGame = new ForcaGame();
+        string[] wordList = forcaGame.getWordList();
+        List<char> guessesList = forcaGame.getGuessesList();
 
-        char[] randomWordLetters = randomWord.ToCharArray();
-        char[] guessedLetters = hiddenLetters(randomWordLetters);
+        do {
+            Random random = new Random();
+            int indexWord = random.Next(wordList.Length);
+            string randomWord = wordList[indexWord];
 
-        startGame(randomWord, guessedLetters);
+            char[] randomWordLetters = randomWord.ToCharArray();
+            char[] guessedLetters = HiddenLetters(randomWordLetters);
+
+            playAgainInput = "";
+            StartGame(randomWord, guessedLetters, guessesList);
+
+        } while (playAgainInput.Equals("S"));
 
     }
-    public static void startGame(string randomWord, char[] guessedLetters) {
+    public static void StartGame(string randomWord, char[] guessedLetters, List<char> guessesList) {
 
-        while (guessesNumber > 0) {
+        while (MAX_GUESSES > 0) {
 
-            updateDisplay(randomWord, guessedLetters);
+            UpdateDisplay(randomWord, guessedLetters, guessesList);
 
-            Console.Write("\n\nDigite somente uma Letra: ");
-            ConsoleKeyInfo inputLetter = Console.ReadKey();
+            ConsoleKeyInfo inputLetter = GetInputLetter();
 
-            bool isLetter = checkLetter(inputLetter.KeyChar);
-
-            char formattedLetter = formatLetter(inputLetter);
+            bool isLetter = CheckLetterValidity(inputLetter.KeyChar);
 
             if (isLetter) {
-                if (randomWord.Contains(formattedLetter)) {
-                    for (int i = 0; i < randomWord.Length; i++) {
-                        if (randomWord[i].Equals(formattedLetter)) {
-                            guessedLetters[i] = formattedLetter;
-                        }
+                char formattedLetter = FormatLetter(inputLetter);
 
-                    }
-                    addLettersGuesses(formattedLetter);
+                if (randomWord.Contains(formattedLetter)) {
+                    AddGuessesList(formattedLetter, guessesList);
+                    UpdateGuessedLetters(randomWord, formattedLetter, guessedLetters);
                 }
-                else if (!guessList.Contains(formattedLetter)) {
-                    decrementGuesses();
-                    addLettersGuesses(formattedLetter);
+                else if (!guessesList.Contains(formattedLetter)) {
+                    DecrementGuesses();
+                    AddGuessesList(formattedLetter, guessesList);
                 }
                 else {
-                    addLettersGuesses(formattedLetter);
+                    AddGuessesList(formattedLetter, guessesList);
                 }
 
             }
 
-            if (guessesNumber == 0) {
+            if (MAX_GUESSES == 0) {
                 Console.ForegroundColor = ConsoleColor.Red;
-                updateDisplay(randomWord, guessedLetters);
+                UpdateDisplay(randomWord, guessedLetters, guessesList);
                 Console.WriteLine("\n\n\n");
                 Console.WriteLine("\t\tVocê perdeu!!!. Excedeu o número de Palpites.");
+                PlayAgain(guessesList);
                 return;
             }
 
 
             if (randomWord.SequenceEqual(guessedLetters)) {
                 Console.ForegroundColor = ConsoleColor.Green;
-                updateDisplay(randomWord, guessedLetters);
+                UpdateDisplay(randomWord, guessedLetters, guessesList);
                 Console.WriteLine("\n\n\n");
                 Console.WriteLine("\tParabéns você acertou a palavra.");
+                PlayAgain(guessesList);
                 return;
 
             }
@@ -76,33 +89,33 @@ public class Program {
 
     }
 
-    public static void updateDisplay(string randomWord, char[] guessedLetters) {
+    public static void UpdateDisplay(string randomWord, char[] guessedLetters, List<char> guessesList) {
         Console.Clear();
         Console.WriteLine("\n\t ************* DESAFIO FORCA ****************** \t");
         Console.WriteLine("\n");
         Console.WriteLine("\t Dica: COR");
-        Console.WriteLine("\n\t Uma cor com " + randomWord.Length + " LETRAS");
-        Console.WriteLine("\n\t Número de Palpites: " + guessesNumber);
-        showGuesses();
+        Console.WriteLine($"\n\t Uma cor com {randomWord.Length} LETRAS");
+        Console.WriteLine($"\n\t Número de Palpites: {MAX_GUESSES}");
+        ShowGuesses(guessesList);
         Console.WriteLine("\n\n");
-        showGuessedLetters(guessedLetters);
+        ShowGuessedLetters(guessedLetters);
 
     }
-
-    public static char[] hiddenLetters(char[] randomWordLetters) {
+    public static char[] HiddenLetters(char[] randomWordLetters) {
         return new string('_', randomWordLetters.Length).ToCharArray();
     }
 
-    public static void showGuessedLetters(char[] guessedLetters) {
-        for (int i = 0; i < guessedLetters.Length; i++) {
-            Console.Write("\t" + guessedLetters[i]);
-        }
+    public static ConsoleKeyInfo GetInputLetter() {
+        Console.Write("\n\nDigite somente uma Letra: ");
+        ConsoleKeyInfo inputLetter = Console.ReadKey();
+        return inputLetter;
     }
 
-    public static bool checkLetter(char inputLetter) {
+    public static bool CheckLetterValidity(char inputLetter) {
         if (!char.IsLetter(inputLetter)) {
             Console.ForegroundColor = ConsoleColor.Red;
-            Console.WriteLine("\nLetra Inválida!!!. Pressione ENTER pra continuar....");
+            Console.WriteLine("\nEntrada inválida!!! Por favor, insira uma letra.");
+            Console.WriteLine("\nPressione ENTER para continuar...");
             Console.ResetColor();
             Console.ReadLine();
             return false;
@@ -111,16 +124,31 @@ public class Program {
         return true;
     }
 
-    public static char formatLetter(ConsoleKeyInfo inputLetter) {
+    public static char FormatLetter(ConsoleKeyInfo inputLetter) {
         string str = new string(inputLetter.KeyChar, 1);
         char formatLetter = char.Parse(str.ToUpper());
         return formatLetter;
     }
 
+    public static void UpdateGuessedLetters(string randomWord, char formattedLetter, char[] guessedLetters) {
+        for (int i = 0; i < randomWord.Length; i++) {
+            if (randomWord[i].Equals(formattedLetter)) {
+                guessedLetters[i] = formattedLetter;
+            }
 
-    public static void addLettersGuesses(char formattedLetter) {
-        if (!guessList.Contains(formattedLetter)) {
-            guessList.Add(formattedLetter);
+        }
+    }
+
+    public static void ShowGuessedLetters(char[] guessedLetters) {
+        foreach (char letter in guessedLetters) {
+            Console.Write($"\t{letter}");
+        }
+    }
+
+
+    public static void AddGuessesList(char formattedLetter, List<char> guessesList) {
+        if (!guessesList.Contains(formattedLetter)) {
+            guessesList.Add(formattedLetter);
 
         }
         else {
@@ -132,16 +160,51 @@ public class Program {
         }
     }
 
-    public static void showGuesses() {
+    public static void ShowGuesses(List<char> guessesList) {
         Console.Write("\n\t Tentativas: ");
-        for (int i = 0; i < guessList.Count; i++) {
-            Console.Write(guessList[i] + ", ");
+        for (int i = 0; i < guessesList.Count; i++) {
+            Console.Write(guessesList[i] + ", ");
         }
 
     }
 
-    public static int decrementGuesses() {
-        return guessesNumber--;
+    public static int DecrementGuesses() {
+        return MAX_GUESSES--;
+    }
+
+    public static void ResetGame(List<char> guessesList) {
+        Console.Clear();
+        Console.ResetColor();
+        guessesList.Clear();
+        MAX_GUESSES = 6;
+
+    }
+
+
+    public static void PlayAgain(List<char> guessesList) {
+    
+        while (playAgainInput != "S") {
+            Console.Write("\n\tDeseja jogar Novamente S (SIM) ou N (NÃO)? ENTER para confirmar ");
+            playAgainInput = Console.ReadLine().ToUpper();
+
+
+            if (playAgainInput == "N") {
+                Environment.Exit(0);
+
+            }
+
+            else if (playAgainInput != "S") {
+                Console.WriteLine("\nOpção Inválida. Pressione Enter para voltar.");
+                Console.ReadLine();
+
+            }
+            else {
+                ResetGame(guessesList);
+            }
+
+        }
+
+
     }
 
 
